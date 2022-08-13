e879696e0f3330e0024288244cd2502b5860bccb
Alvaro <dralvphd2@gmail.com>
2022-08-13 16:21:14 -0300

    Criando o componente de card

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './shared/components/pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './shared/components/pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonListComponent
+    PokemonListComponent,
+    PokemonCardComponent
   ],
   imports: [
     BrowserModule,
